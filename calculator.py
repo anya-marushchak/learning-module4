26bcf8c9ee32ab0c5c9586513ea7a82ea44677f4
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-02 15:23:29 +0100

    Add calculator.py

---
+++ calculator.py
@@ -0,0 +1,39 @@
+#adding function
+def add(a,b):
+    return (a+b)
+#substracting function
+def sub(a,b):
+    return(a-b)
+#multiplying function
+def mul(a,b):
+    return(a*b)
+#deviding function
+def dev(a,b):
+    return(a/b)
+
+action = int(input( "Please, choose the action by using the following number:1 Adding,2 Substraction, 3 Multiplication, 4 Devision:"))
+a = int(input("First number:"))
+b = int(input("Second number:"))
+
+if action == 1:
+    result = add(a,b)
+    result = "The result is " + str(result)
+    print ("Adding of " + str(a) + " and " + str(b))
+    print (result)
+elif action == 2:
+    result = sub(a,b)
+    result = "The result is " + str(result)
+    print ("Substracting of " + str(a) + " and " + str(b))
+    print (result)
+elif action == 3:
+    result = mul(a,b)
+    result = "The result is " + str(result)
+    print ("Multipying " + str(a) + " to " + str(b))
+    print (result)
+elif action == 4:
+    result = dev(a,b)
+    result = "The result is " + str(result)
+    print ("Deviding " + str(a) + " to " + str(b))
+    print (result)
+else:
+    print("Invalid input")
