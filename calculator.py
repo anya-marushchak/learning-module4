cec0939e685d8222b6171484667b0aea74cf6036
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-02 15:38:23 +0100

    Add logging module

--- calculator.py
+++ calculator.py
@@ -15,25 +15,27 @@ action = int(input( "Please, choose the action by using the following number:1 A
 a = int(input("First number:"))
 b = int(input("Second number:"))
 
+import logging
+logging.basicConfig(level=logging.INFO)
 if action == 1:
     result = add(a,b)
     result = "The result is " + str(result)
-    print ("Adding of " + str(a) + " and " + str(b))
+    logging.info("Adding of " + str(a) + ' and '+ str(b))
     print (result)
 elif action == 2:
     result = sub(a,b)
     result = "The result is " + str(result)
-    print ("Substracting of " + str(a) + " and " + str(b))
+    logging.info("Substraction of " + str(a) + ' and '+ str(b))
     print (result)
 elif action == 3:
     result = mul(a,b)
     result = "The result is " + str(result)
-    print ("Multipying " + str(a) + " to " + str(b))
+    logging.info("Multiplication of " + str(a) + ' and '+ str(b))
     print (result)
 elif action == 4:
     result = dev(a,b)
     result = "The result is " + str(result)
-    print ("Deviding " + str(a) + " to " + str(b))
+    logging.info("Devision of " + str(a) + ' and '+ str(b))
     print (result)
 else:
     print("Invalid input")
